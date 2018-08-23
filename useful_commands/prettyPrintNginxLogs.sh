6d8890b55508cda2dd0f9e8430e7fe1fd46284cb
hamednourhani <itstarir@gmail.com>
2018-08-23 17:05:08 +0430

    nginxLogPretty command added

--- useful_commands/prettyPrintNginxLogs.sh
+++ useful_commands/prettyPrintNginxLogs.sh
@@ -1,4 +1,5 @@
 sed 's/\[nginx-\w*\]//' sample_files/nginx.log | while read line; do echo "$line" | json_pp ;done
 
+
 tail -F sample_files/nginx.log | while read line; do echo "$line" | awk 'print{$2}' | json_pp ;done
 
